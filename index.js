4e11c6aedf8c20b907bd0f389ca1203391f5fdf4
himanshu <=>
2019-07-28 20:12:48 +0530

    ===>

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const ogs = require('open-graph-scraper');
 const firebase = require('firebase');
 
 const token = '900890771:AAHwYVSifgMid9cy-_DNmqWtZKYpmF4PgmI';
-const bot = new TelegramBot(token, {polling: true}); 
+const bot = new TelegramBot(token, { polling: true });
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyC4vDkwKbxaYxELykJU73zv8w4SEYxOyME",
-    authDomain: "chatbot-98e06.firebaseapp.com",
-    databaseURL: "https://chatbot-98e06.firebaseio.com",
-    projectId: "chatbot-98e06",
-    storageBucket: "chatbot-98e06.appspot.com",
-    messagingSenderId: "977296706537",
-    appId: "1:977296706537:web:ab72fe0ced036a41"
+  apiKey: "AIzaSyC4vDkwKbxaYxELykJU73zv8w4SEYxOyME",
+  authDomain: "chatbot-98e06.firebaseapp.com",
+  databaseURL: "https://chatbot-98e06.firebaseio.com",
+  projectId: "chatbot-98e06",
+  storageBucket: "chatbot-98e06.appspot.com",
+  messagingSenderId: "977296706537",
+  appId: "1:977296706537:web:ab72fe0ced036a41"
 });
 
 const ref = firebase.database().ref();
@@ -20,44 +20,130 @@ const sitesRef = ref.child("sites");
 
 let siteUrl;
 
-bot.onText(/\/bookmark (.+)/, (msg, match) => {
+const complaint = {
+  category: "",
+  description: "",
+  contact: "",
+  timesloat: "",
+  key: ""
+}
+
+function askContact(msg) {
+  // var option = {
+  //   "reply_markup": {
+  //     "one_time_keyboard": true,
+  //     "keyboard": [[{
+  //       text: "My phone number",
+  //       request_contact: true
+  //     }], ["Cancel"]]
+  //   }
+  // };
+  bot.sendMessage(msg.chat.id, "How can we contact you?").then((contact) => {
+    console.log({ contact });
+    // handle user phone
+  });
+}
+function askTimeSlot(msg) {
+  var option = {
+    "parse_mode": "Markdown",
+  };
+  bot.sendMessage(msg.chat.id, "What time slot do u prefer (hh:mm - hh:mm)?", option).then(() => {
+    // handle user phone
+  })
+}
+bot.onText(/\/newcomplaints/, (msg, match) => {
   siteUrl = match[1];
-  bot.sendMessage(msg.chat.id,'Got it, in which category?', {
+  console.log("no", siteUrl)
+  var option = {
+    "parse_mode": "Markdown",
     reply_markup: {
       inline_keyboard: [[
         {
-          text: 'Development',
-          callback_data: 'development'
-        },{
-          text: 'Music',
-          callback_data: 'music'
-        },{
-          text: 'Cute monkeys',
-          callback_data: 'cute-monkeys'
+          text: 'Plumber',
+          callback_data: 'plumber'
+        }, {
+          text: 'Electrition',
+          callback_data: 'electrition'
+        }, {
+          text: 'Carpenter',
+          callback_data: 'carpenter'
         }
       ]]
     }
-  });
+  };
+  bot.sendMessage(msg.chat.id, `Got it ${msg.chat.first_name}, in which category?`, option)
 });
 
-bot.on("callback_query", (callbackQuery) => {
-    const message = callbackQuery.message;
-    ogs({'url': siteUrl}, function (error, results) {
-      if(results.success) {
-        sitesRef.push().set({
-          name: results.data.ogSiteName,
-          title: results.data.ogTitle,
-          description: results.data.ogDescription,
-          url: siteUrl,
-          thumbnail: results.data.ogImage.url,
-          category: callbackQuery.data
-        });
-        bot.sendMessage(message.chat.id,'Added \"' + results.data.ogTitle +'\" to category \"' + callbackQuery.data + '\"!')
-  } else {
-        sitesRef.push().set({
-          url: siteUrl
-        });
-        bot.sendMessage(message.chat.id,'Added new website, but there was no OG data!');
-      }
-    });
+bot.onText(/\/mycomplaints/, (msg, match) => {
+  siteUrl = match[1];
+  console.log("sadasd");
+ const j =  ref.child("sites").once('value').
+ then(snapshot => {
+  values  = Object.values(snapshot.val());
+  val1 = values.filter(v => {
+    v.name != 'Kavita';
   });
+  console.log(val1)
+  let str = "";
+  val1.forEach(el => {
+    el => str += el.description;
+  });  
+  console.log(val1);
+  bot.sendMessage(msg.chat.id, `details ===> ${str}`)
+ }
+  );
+ 
+  bot.sendMessage(msg.chat.id, `fetching details`)
+});
+
+bot.on("contact",(msg)=>{
+  complaint.contact = msge.contact  
+})
+bot.on('message', (msg) => {
+  console.log(msg.text);
+  if (msg.text === 'mycomplaints') return;
+  console.log({complaint});
+  if (complaint.category && !complaint.description) {
+    complaint.description = msg.text;
+    console.log(complaint);
+    askContact(msg);
+    return;
+  }
+
+  if (complaint.category && complaint.description && !complaint.contact) {
+    complaint.contact = msg.text;
+    askTimeSlot(msg);
+    return;
+  }
+ 
+
+  if (complaint.category && complaint.description && complaint.contact) {
+    complaint.timesloat = msg.text;
+    sitesRef.push().set({
+      name: msg.chat.first_name,
+      description: complaint.description,
+      timesloat: complaint.timesloat,
+      category: complaint.category,
+      chatId: msg.chat.id,
+     
+    });
+    bot.sendMessage(msg.chat.id, 'your complaint registered.');
+    
+    return;
+  }
+  else if (complaint.category && complaint.description && complaint.contact && !complaint.timesloat) {
+    askTimeSlot(msg);
+   // return;
+  }
+});
+
+
+bot.on("callback_query", (callbackQuery) => {
+  const msg = callbackQuery.message;
+  if (['plumber', 'carpenter', 'electrition'].indexOf(callbackQuery.data) > -1) {
+    complaint.category = callbackQuery.data;
+  }
+  bot.sendMessage(msg.chat.id, `Please endter briefly help u needed?`).then((msg) => {
+      
+    })
+});
